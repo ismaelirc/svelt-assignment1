f302313a0edc4e02c7a8c605544930b3dc7f121e
Ismael Ricardo Costa <ismaelirc@gmail.com>
2020-03-16 13:17:51 -0300

    Assignment 1

---
+++ src/Component.svelte
@@ -0,0 +1,24 @@
+<script>
+    let goal = "";  
+    let red = false;
+
+    function setGoal(event){
+        const val = event.target.value;
+        goal = val;
+
+        if(val.indexOf("!") >= 0){
+            red = true;
+        }
+
+    }
+
+</script>
+<style>
+    .h1-color{
+        color: red;
+    }
+</style>
+<div id="InputH1">
+    <h1 class={red ? "h1-color": ""}>{goal}</h1>
+    <input type="text" value={goal} on:input={setGoal}/>
+</div>
