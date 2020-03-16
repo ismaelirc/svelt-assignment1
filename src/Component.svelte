3a3296ab6a755eb1d57621e0b1a93a7d066969e0
Ismael Ricardo Costa <ismaelirc@gmail.com>
2020-03-16 13:59:08 -0300

    second solution

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,24 +1,16 @@
 <script>
+    
     let goal = "";  
-    let red = false;
-
-    function setGoal(event){
-        const val = event.target.value;
-        goal = val;
-
-        if(val.indexOf("!") >= 0){
-            red = true;
-        }
-
-    }
+    
+    $: shouldRed = goal.includes('!');
 
 </script>
 <style>
-    .h1-color{
+    .h-color{
         color: red;
     }
 </style>
 <div id="InputH1">
-    <h1 class={red ? "h1-color": ""}>{goal}</h1>
-    <input type="text" value={goal} on:input={setGoal}/>
+    <h1 class:h-color={shouldRed}>{goal}</h1>
+    <input type="text" bind:value={goal} />
 </div>
